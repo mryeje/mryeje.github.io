b01301a057bbc7604518e5d0bb11c00dc603294f
mryeje <mryeje@gmail.com>
2024-02-29 10:53:33 -0400

    Add files via upload

---
+++ drummotor_script.js
@@ -0,0 +1,41 @@
+// Sketchfab Viewer API: Start/Stop the viewer
+var version = '1.12.1';
+var uid = '70f7fc657ed44168a672eef1d823acf8';
+var iframe = document.getElementById('api-frame');
+var client = new window.Sketchfab(version, iframe);
+var error = function error() {
+  console.error('Sketchfab API error');
+};
+var success = function success(api) {
+  api.start(function () {
+    api.addEventListener('viewerready', function () {
+      api.getSceneGraph(function (err, result) {
+        if (err) {
+          console.log('Error getting nodes');
+          return;
+        }
+        // get the id from that log
+        console.log(result);
+      });
+
+	  
+
+		
+				  
+    });
+  });
+};
+client.init(uid, {
+  success: success,
+  error: error,
+  autostart: 1,
+  preload: 1
+});
+//////////////////////////////////
+// GUI Code
+//////////////////////////////////
+
+
+//////////////////////////////////
+// GUI Code end
+//////////////////////////////////
